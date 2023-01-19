b6fa29b86dadb43929877811d5b023464b2bcf5d
keilokimnida <tkl48leon@gmail.com>
2023-01-19 21:46:07 +0800

    [fix] fix visual bug in events page

--- components/shared/EventListItem/EventListItemSkeleton.tsx
+++ components/shared/EventListItem/EventListItemSkeleton.tsx
@@ -4,10 +4,15 @@ import { Skeleton } from "@mui/material";
 const EventListItemSkeleton = (): JSX.Element => (
     <div className="c-Event-list-item-skeleton">
         <div className="c-Event-list-item-skeleton__Img c-Img">
-            <Skeleton variant="rectangular" width={200} height={200} />
+            <Skeleton
+                className="c-Img__Raw"
+                variant="rectangular"
+                width={200}
+                height={200}
+            />
         </div>
         <div className="c-Event-list-item-skeleton__Desc c-Desc">
-            <Skeleton width={300} height={30} />
+            <Skeleton width={250} height={30} />
             <Skeleton width={150} height={30} />
             <div className="c-Desc__Date-and-time c-Date-and-time">
                 <Skeleton className="c-Date" width={100} height={30} />
